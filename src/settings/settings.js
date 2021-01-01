1577f0325f82d1ad5fec0843881381349fb6acac
Pratyush Jain <pratyush.jain2001@gmail.com>
2021-01-01 20:42:35 +0530

    mail done

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -4,3 +4,9 @@ $(document).ready(function() {
     $(".tab-content").find('.tab-pane').eq($(this).index()).toggle({ direction: "top" }, 2000);
   });
 });
+
+
+function send() {
+  var link = 'mailto:enlist.feedback@gmail.com';
+  window.location.href = link;
+  };
