af0a990c318576037683879d64f11210ad98683c
Rahul Mansharamani <rahul.mansharamani.btech2019@sitpune.edu.in>
2021-05-14 04:50:39 +0530

    bin route added

--- src/controller/service.controller.js
+++ src/controller/service.controller.js
@@ -1,6 +1,6 @@
-const { delete_a_task } = require("../model/services");
+const { trash_a_task, delete_a_task, get_all_deleted_tasks } = require("../model/services");
 
-const deleteTask = async (req, res) => {
+const trash_Task = async (req, res) => {
   if (!req.decodedClaims) {
     res.redirect("/login");
   } else {
@@ -21,7 +21,7 @@ const deleteTask = async (req, res) => {
     //res.json("success");
 
     try {
-      await delete_a_task(user, req.body, req.params.group);
+      await trash_a_task(user, req.body, req.params.group);
 
       res.json("Success");
     } catch (error) {
@@ -30,4 +30,79 @@ const deleteTask = async (req, res) => {
   }
 };
 
-module.exports = { deleteTask };
+const deleteTask = async (req, res) => {
+  if (!req.decodedClaims) {
+    res.redirect("/login");
+  } else {
+    let group;
+    console.log(req._payload);
+
+    if (req.params.group == "class") group = req._payload._class;
+    if (req.params.group == "division") group = req._payload._division;
+    if (req.params.group == "personal") group = "Pvt";
+
+    const user = {
+      name: req.decodedClaims.name,
+      email: req.decodedClaims.email,
+      uid: req.decodedClaims.uid,
+      picture: req.decodedClaims.picture,
+    };
+
+    console.log(req.body);
+    console.log("Group :", group);
+    //res.json("success");
+
+    try {
+      await delete_a_task(user, req.body, group);
+
+      res.json("Success");
+    } catch (error) {
+      res.json("Failed to delete");
+    }
+  }
+};
+
+const get_deleted_tasks = async (req, res) => {
+  if (!req.decodedClaims) {
+    res.redirect("/login");
+  } else {
+    let group;
+    console.log(req.decodedClaims);
+    const user = { name: req.decodedClaims.name, email: req.decodedClaims.email, picture: req.decodedClaims.picture };
+
+    const metadata = req._payload;
+    if (req.params.group == "class") {
+      group = req._payload._class;
+      metadata.standard = req.params.group;
+    }
+    if (req.params.group == "division") {
+      group = req._payload._division;
+      metadata.standard = req.params.group;
+    }
+
+    if (req.params.group == "personal") {
+      group = "Pvt";
+      metadata.standard = req.params.group;
+    }
+
+    const deletedTasks = await get_all_deleted_tasks(req.decodedClaims.uid);
+
+    metadata.group = group;
+
+    // tasks.map((task) => {
+    //   task.date = moment.unix(task.date).format("DD MMMM YYYY");
+    // });
+
+    //console.log(impTasks);
+
+    deletedTasks.map((group) => {
+      for (const tasks in group) {
+        Object.values(group[tasks]).forEach((task) => console.log(task.title));
+      }
+    });
+
+    res.render("pages/services/bin", { user, deletedTasks, metadata });
+  }
+};
+
+module.exports = { trash_Task, deleteTask, get_deleted_tasks };
--- src/model/services.js
+++ src/model/services.js
@@ -5,7 +5,7 @@ const { moveFbRecord } = require("../utils/moveNode");
 
 const taskRef = db.ref("To-Do-List");
 
-const delete_a_task = async (user, task, group) => {
+const trash_a_task = async (user, task, group) => {
   const uniquekey = task.key;
 
   console.log(uniquekey);
@@ -14,4 +14,35 @@ const delete_a_task = async (user, task, group) => {
   await completed_to_delete.remove();
 };
 
-module.exports = { delete_a_task };
+const delete_a_task = async (user, task, group) => {
+  const uniquekey = task.key;
+
+  console.log(uniquekey);
+  const task_ref = db.ref("To-Do-List/" + user.uid + "/" + group + "/Task" + uniquekey);
+  const new_task_ref = db.ref("To-Do-List/" + user.uid + "/" + "Deleted" + "/" + group + "/Task" + uniquekey);
+
+  await moveFbRecord(task_ref, new_task_ref);
+};
+
+const get_all_deleted_tasks = async (uid) => {
+  let group_task = [];
+
+  await db.ref("To-Do-List/" + uid + "/" + "Deleted").once("value", function (snapshot) {
+    console.log("Snapshot: ", snapshot);
+
+    snapshot.forEach(function (childSnapshot) {
+      let obj = {};
+      var childKey = childSnapshot.key;
+      var childData = childSnapshot.val();
+      //console.log(childData);
+      obj[childKey] = childData;
+      group_task.push(obj);
+      // console.log("Key: ", childKey);
+      // console.log("Data: ", childData);
+    });
+  });
+
+  return group_task;
+};
+
+module.exports = { trash_a_task, delete_a_task, get_all_deleted_tasks };
