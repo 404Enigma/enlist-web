722f3edf4ed3fd2786002d30ab1a074a2af2360c
Rahul Mansharamani <rahul.mansharamani.btech2019@sitpune.edu.in>
2021-07-19 23:11:17 +0530

    reviews displayed

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { add_review } = require("../src/controller/reviews.controller");
+const { add_review, get_reviews } = require("../src/controller/reviews.controller");
 
-router.get("/reviews", (req, res) => {
-  res.render("pages/review");
-});
-
+router.get("/reviews", get_reviews);
 router.post("/reviews", add_review);
 
 module.exports = router;
--- src/controller/reviews.controller.js
+++ src/controller/reviews.controller.js
@@ -2,15 +2,38 @@ const admin = require("../../src/db/db");
 const db = admin.firestore();
 const reviewRef = db.collection("Reviews");
 
+var moment = require("moment");
+moment().format();
+
 const add_review = async (req, res) => {
   const { name, email, text, rating } = req.body;
 
   try {
-    await reviewRef.doc(email).set({ name: name, email: email, description: text, rating: rating });
+    await reviewRef.doc(email).set({ name: name, email: email, description: text, rating: rating, created_at: moment().unix() });
     return res.json("success");
   } catch (error) {
     console.log(error);
   }
 };
 
-module.exports = { add_review };
+const get_reviews = async (req, res) => {
+  let reviews_data = [];
+  let review_obj = {};
+  const snapshot = await reviewRef.orderBy("created_at").get();
+  snapshot.forEach((doc) => {
+    review_obj = doc.data();
+
+    review_obj.created_at = moment.unix(review_obj.created_at).format("DD MMMM YYYY");
+
+    reviews_data.push(review_obj);
+  });
+
+  console.log(reviews_data);
+
+  res.render("pages/review", { reviews_data });
+};
+
+// const a = moment.unix(1626713565951).format("DD MMMM YYYY");
+// console.log(a);
+
+module.exports = { add_review, get_reviews };
